731e2cc65503c7b1035f8cc7ca25dd890f371882
Ardenneslol <ardenneslul@gmail.com>
2019-03-09 15:23:52 -0600

    Updates

--- src/haven/StudyWnd.java
+++ src/haven/StudyWnd.java
@@ -16,14 +16,14 @@ public class StudyWnd extends GameUI.Hidewnd {
 			info.reqdestroy();
 		}
 		final InventoryProxy invp = new InventoryProxy(inventory);
-		study = add(invp, 0, 20);
-		info = add(new StudyInfo(new Coord(study.sz.x, 50), inventory), 0, study.c.y + study.sz.y + 5);
+		study = add(invp, 0, 0);
+		info = add(new StudyInfo(new Coord(study.sz.x, 66), inventory), 0, study.c.y + study.sz.y + 5);
 		pack();
 	}
 
 	private static class StudyInfo extends Widget {
 		public Widget study;
-		public int texp, tw, tenc;
+		public int texp, tw, tenc, tlph;
 		private final Text.UText<?> texpt = new Text.UText<Integer>(Text.std) {
 			public Integer value() {
 				return (texp);
@@ -47,6 +47,15 @@ public class StudyWnd extends GameUI.Hidewnd {
 				return (Integer.toString(tenc));
 			}
 		};
+		private final Text.UText<?> tlphr = new Text.UText<Integer>(Text.std) {
+			public Integer value() {
+				return (tlph);
+			}
+
+			public String text(Integer v) {
+				return (Integer.toString(tlph));
+			}
+		};
 
 		private StudyInfo(Coord sz, Widget study) {
 			super(sz);
@@ -54,10 +63,12 @@ public class StudyWnd extends GameUI.Hidewnd {
 			add(new Label("Attention:"), 2, 2);
 			add(new Label("Experience cost:"), 2, 18);
 			add(new Label("Learning points:"), 2, 34);
+			add(new Label("LP/HR:"), 2, 50);
 		}
 
 		private void upd() {
 			int texp = 0, tw = 0, tenc = 0;
+			double tlph = 0;
 			for (GItem item : study.children(GItem.class)) {
 				try {
 					Curiosity ci = ItemInfo.find(Curiosity.class, item.info());
@@ -65,6 +76,7 @@ public class StudyWnd extends GameUI.Hidewnd {
 						texp += ci.exp;
 						tw += ci.mw;
 						tenc += ci.enc;
+						tlph += (ci.exp / (ci.time / 60));
 					}
 				} catch (Loading ignored) {
 				}
@@ -72,6 +84,7 @@ public class StudyWnd extends GameUI.Hidewnd {
 			this.texp = texp;
 			this.tw = tw;
 			this.tenc = tenc;
+			this.tlph = (int) Math.round(tlph);
 		}
 
 		public void draw(GOut g) {
@@ -83,6 +96,8 @@ public class StudyWnd extends GameUI.Hidewnd {
 			g.aimage(tenct.get().tex(), new Coord(sz.x - 4, 18), 1.0, 0.0);
 			g.chcolor(192, 192, 255, 255);
 			g.aimage(texpt.get().tex(), new Coord(sz.x - 4, 34), 1.0, 0.0);
+			g.chcolor(192, 192, 255, 255);
+			g.aimage(tlphr.get().tex(), new Coord(sz.x - 4, 50), 1.0, 0.0);
 		}
 	}
 }
