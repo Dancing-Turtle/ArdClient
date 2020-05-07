4e3a9d924db19ff17b7c682d9ab4854601d196eb
Vareckeska <talon0030@hotmail.com>
2020-05-07 17:32:25 -0400

    Added item-spec utility for getting info from things using gsprites.

--- src/haven/OwnerContext.java
+++ src/haven/OwnerContext.java
@@ -93,4 +93,8 @@ public interface OwnerContext {
             });
         }
     }
+
+    public static final ClassResolver<UI> uictx = new ClassResolver<UI>()
+            .add(Glob.class, ui -> ui.sess.glob)
+            .add(Session.class, ui -> ui.sess);
 }
