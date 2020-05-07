4615c17b7790a6516b8bfa08493790689811393d
Vareckeska <talon0030@hotmail.com>
2020-05-07 16:26:57 -0400

    Send more correct key specification to globtype.

--- src/haven/UI.java
+++ src/haven/UI.java
@@ -466,8 +466,12 @@ public class UI {
                 return;
         }
         }
-        if (!root.keydown(ev))
-            root.globtype((char) 0, ev);
+        if(!root.keydown(ev)) {
+            char key = ev.getKeyChar();
+            if(key == ev.CHAR_UNDEFINED)
+                key = 0;
+            root.globtype(key, ev);
+        }
     }
 
     public void keyup(KeyEvent ev) {
