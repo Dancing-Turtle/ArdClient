43aa19571f5a7424b5e5abbda6ac56296f92f514
Vareckeska <talon0030@hotmail.com>
2020-05-07 17:09:38 -0400

    Add ability to track information per-object in fightview.

--- src/haven/Fightview.java
+++ src/haven/Fightview.java
@@ -29,10 +29,7 @@ package haven;
 
 
 import java.awt.Color;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Fightview extends Widget {
     public static Tex bg = Theme.tex("bosq");
@@ -47,6 +44,7 @@ public class Fightview extends Widget {
     static Coord cpursc = new Coord(cavac.x - 75, cgivec.y + 35);
     public LinkedList<Relation> lsrel = new LinkedList<Relation>();
     public Relation current = null;
+    public final Map<Long, Widget> obinfo = new HashMap<>();
     public static String ttretaincur, ttretain;
     public List <Relation> notcurrent;
     public Indir<Resource> blk, batk, iatk;
@@ -255,6 +253,48 @@ public class Fightview extends Widget {
         } else {
             super.addchild(child, args);
         }
+    }
+
+    /* XXX? It's a bit ugly that there's no trimming of obinfo, but
+     * it's not obvious that one really ever wants it trimmed, and
+     * it's really not like it uses a lot of memory. */
+    public Widget obinfo(long gobid, boolean creat) {
+        synchronized(obinfo) {
+            Widget ret = obinfo.get(gobid);
+            if((ret == null) && creat)
+                obinfo.put(gobid, ret = new AWidget());
+            return(ret);
+        }
+    }
+
+    public void tick(double dt) {
+        super.tick(dt);
+        for(Relation rel : lsrel) {
+            Widget inf = obinfo(rel.gobid, false);
+            if(inf != null)
+                inf.tick(dt);
+        }
+    }
+
+    public <T extends Widget> T obinfo(long gobid, Class<T> cl, boolean creat) {
+        Widget cnt = obinfo(gobid, creat);
+        if(cnt == null)
+            return(null);
+        T ret = cnt.getchild(cl);
+        if((ret == null) && creat) {
+            try {
+                ret = Utils.construct(cl.getConstructor());
+            } catch(NoSuchMethodException e) {
+                throw(new RuntimeException(e));
+            }
+            cnt.add(ret);
+        }
+        return(ret);
+    }
+
+    public static interface ObInfo {
+        public default int prio() {return(1000);}
+        public default Coord2d grav() {return(new Coord2d(0, 1));}
     }
 
     private void setcur(Relation rel) {
