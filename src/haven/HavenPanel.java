0e50d844a8a85c4e4c67530589d07d3c372f4f42
Vareckeska <talon0030@hotmail.com>
2020-05-07 19:12:57 -0400

    Lazy fix

--- src/haven/HavenPanel.java
+++ src/haven/HavenPanel.java
@@ -28,7 +28,10 @@ package haven;
 
 import com.jogamp.opengl.util.awt.Screenshot;
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.GraphicsConfiguration;
+import java.awt.Toolkit;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
@@ -166,6 +169,7 @@ public class HavenPanel extends GLCanvas implements Runnable, Console.Directory,
                             BGL gl = g.gl;
                             gl.glColor3f(1, 1, 1);
                             gl.glPointSize(4);
+                            // gl.joglSetSwapInterval(1);
                             gl.joglSetSwapInterval((aswap = iswap) ? 1 : 0);
                             gl.glEnable(GL.GL_BLEND);
                             //gl.glEnable(GL.GL_LINE_SMOOTH);
@@ -173,10 +177,10 @@ public class HavenPanel extends GLCanvas implements Runnable, Console.Directory,
                             if (g.gc.glmajver >= 2)
                                 gl.glBlendEquationSeparate(GL.GL_FUNC_ADD, GL2.GL_MAX);
                             if (g.gc.havefsaa()) {
-                    /* Apparently, having sample
-                     * buffers in the config enables
-					 * multisampling by default on
-					 * some systems. */
+                                /* Apparently, having sample
+                                 * buffers in the config enables
+                                 * multisampling by default on
+                                 * some systems. */
                                 g.gl.glDisable(GL.GL_MULTISAMPLE);
                             }
                             GOut.checkerr(gl);
@@ -219,6 +223,11 @@ public class HavenPanel extends GLCanvas implements Runnable, Console.Directory,
             public void dispose(GLAutoDrawable d) {
             }
         });
+    }
+
+    @Override
+    public void setmousepos(Coord c) {
+
     }
 
     public static abstract class OrthoState extends GLState {
@@ -357,6 +366,7 @@ public class HavenPanel extends GLCanvas implements Runnable, Console.Directory,
         }
 
         if (Config.dbtext) {
+            // int y = h - 165;
             int y = h - 190;
             FastText.aprintf(g, new Coord(10, y -= 15), 0, 1, "FPS: %d (%d%%, %d%% idle)", fps, (int)(uidle * 100.0), (int)(ridle * 100.0));
             Runtime rt = Runtime.getRuntime();
@@ -416,7 +426,7 @@ public class HavenPanel extends GLCanvas implements Runnable, Console.Directory,
             g.image(tt, pos);
         }
         ui.lasttip = tooltip;
-        Resource curs = ui.getcurs(mousepos);
+        Resource curs = ui.root.getcurs(mousepos);
         if (cursmode == "awt") {
             if (curs != lastcursor) {
                 try {
@@ -497,7 +507,6 @@ public class HavenPanel extends GLCanvas implements Runnable, Console.Directory,
             gl.setSwapInterval((aswap = iswap) ? 1 : 0);
     }
 
-    private KeyEvent lastpress = null;
     void dispatch() {
         synchronized (events) {
             if (mousemv != null) {
@@ -519,38 +528,11 @@ public class HavenPanel extends GLCanvas implements Runnable, Console.Directory,
                 } else if (e instanceof KeyEvent) {
                     KeyEvent ke = (KeyEvent) e;
                     if (ke.getID() == KeyEvent.KEY_PRESSED) {
-                        InputEvent ne = events.peek();
-                        if(ne instanceof KeyEvent) {
-                            /* This is an extension of the below hack
-                             * to handle dead keys (on Windows). It's
-                             * extremely ugly and error-prone and
-                             * should be dealt with, but I've no idea
-                             * what the alternative would be.*/
-                            KeyEvent nke = (KeyEvent)ne;
-                            if((nke.getID() == KeyEvent.KEY_TYPED) && (nke.getWhen() == ke.getWhen())) {
-                                ke.setKeyChar(nke.getKeyChar());
-                                events.remove();
-                            }
-                        }
                         ui.keydown(ke);
-                        ui.type(ke);
-                        lastpress = ke;
                     } else if (ke.getID() == KeyEvent.KEY_RELEASED) {
                         ui.keyup(ke);
                     } else if (ke.getID() == KeyEvent.KEY_TYPED) {
-                        KeyEvent lp = lastpress;
-                        if((lp != null) && (lp.getKeyChar() == ke.getKeyChar())) {
-                            /* Squelch this event. It certainly is an
-                             * ugly hack, but I just haven't found any
-                             * other way to disambiguate these
-                             * duplicate events. Also, apparently
-                             * getWhen() cannot be completely trusted
-                             * to have the same value for a
-                             * KEY_PRESSED and corresponding KEY_TYPED
-                             * event.*/
-                        } else {
-                            ui.type(ke);
-                        }
+                        ui.type(ke);
                     }
                 }
                 ui.lastevent = Utils.rtime();
@@ -673,6 +655,15 @@ public class HavenPanel extends GLCanvas implements Runnable, Console.Directory,
 
                     frames[framep] = now;
                     waited[framep] = fwaited;
+                    /*
+                    for (int i = 0, ckf = framep, twait = 0; i < frames.length; i++) {
+                        ckf = (ckf - 1 + frames.length) % frames.length;
+                        twait += waited[ckf];
+                        if (now - frames[ckf] > 1000) {
+                            fps = i;
+                            uidle = ((double) twait) / ((double) (now - frames[ckf]));
+                            break;
+                            */
                     {
                         int i = 0, ckf = framep, twait = 0;
                         for(; i < frames.length - 1; i++) {
@@ -684,6 +675,7 @@ public class HavenPanel extends GLCanvas implements Runnable, Console.Directory,
                         fps = (i * 1000) / (now - frames[ckf]);
                         uidle = ((double)twait) / ((double)(now - frames[ckf]));
                     }
+
                     framep = (framep + 1) % frames.length;
 
                     if (curf != null)
@@ -706,22 +698,6 @@ public class HavenPanel extends GLCanvas implements Runnable, Console.Directory,
     public GraphicsConfiguration getconf() {
         return (getGraphicsConfiguration());
     }
-
-    private Robot awtrobot;
-    public void setmousepos(Coord c) {
-        java.awt.EventQueue.invokeLater(() -> {
-            if(awtrobot == null) {
-                try {
-                    awtrobot = new Robot(getGraphicsConfiguration().getDevice());
-                } catch(java.awt.AWTException e) {
-                    return;
-                }
-            }
-            Point rp = getLocationOnScreen();
-            awtrobot.mouseMove(rp.x + c.x, rp.y + c.y);
-        });
-    }
-
 
     private Map<String, Console.Command> cmdmap = new TreeMap<String, Console.Command>();
 
