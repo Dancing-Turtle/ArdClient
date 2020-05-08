35f26c7a171641b35d3ffade4117adc1ce932874
Vareckeska <talon0030@hotmail.com>
2020-05-08 14:16:02 -0400

    Fix NPE and Update Gast

--- src/Gast.java
+++ src/Gast.java
@@ -1,11 +1,9 @@
+import haven.*;
+import haven.ItemInfo.Tip;
+import haven.res.ui.tt.Wear;
 import java.awt.image.BufferedImage;
 
-import haven.ItemInfo.Tip;
-import haven.Resource;
-import haven.RichText;
-import haven.Utils;
-
-public class Gast extends Tip {
+public class Gast extends Tip implements GItem.NumberInfo {
     public final double glut;
     public final double fev;
 
@@ -15,16 +13,25 @@ public class Gast extends Tip {
         this.fev = var4;
     }
 
+    public static ItemInfo mkinfo(Owner var0, Object... var1) {
+        return new Gast(var0, ((Number)var1[1]).doubleValue(), ((Number)var1[2]).doubleValue());
+    }
+
     public BufferedImage tipimg() {
         StringBuilder var1 = new StringBuilder();
-        if(this.glut != 1.0D) {
-            var1.append(String.format(Resource.getLocString(Resource.BUNDLE_LABEL, "Hunger reduction: %s%%\n"), new Object[]{Utils.odformat2(100.0D * this.glut, 1)}));
+        if (this.glut != 1.0D) {
+            var1.append(String.format(Resource.getLocString(Resource.BUNDLE_LABEL, "Hunger reduction: %s%%\n"), Utils.odformat2(100.0D * this.glut, 1)));
         }
 
-        if(this.fev != 1.0D) {
-            var1.append(String.format(Resource.getLocString(Resource.BUNDLE_LABEL, "Food event bonus: %s%%\n"), new Object[]{Utils.odformat2(100.0D * this.fev, 1)}));
+        if (this.fev != 1.0D) {
+            var1.append(String.format(Resource.getLocString(Resource.BUNDLE_LABEL, "Food event bonus: %s%%\n"), Utils.odformat2(100.0D * this.fev, 1)));
         }
 
         return RichText.render(var1.toString(), 0, new Object[0]).img;
+    }
+
+    public int itemnum() {
+        Wear var1 = (Wear)find(Wear.class, this.owner.info());
+        return var1 == null ? 0 : var1.m - var1.d;
     }
 }
--- src/haven/GItem.java
+++ src/haven/GItem.java
@@ -331,15 +331,21 @@ public class GItem extends AWidget implements ItemInfo.SpriteOwner, GSprite.Owne
         }catch(Resource.Loading e){e.printStackTrace(); }
     }
     public Coord size() {
-        Indir<Resource> res = getres().indir();
-        if (res.get() != null && res.get().layer(Resource.imgc) != null) {
-            Tex tex = res.get().layer(Resource.imgc).tex();
-            if(tex == null)
+        try {
+            Indir<Resource> res = getres().indir();
+            if(res.get() != null && res.get().layer(Resource.imgc) != null) {
+                Tex tex = res.get().layer(Resource.imgc).tex();
+                if(tex == null)
+                    return new Coord(1, 1);
+                else
+                    return tex.sz().div(30);
+            } else {
                 return new Coord(1, 1);
-            else
-                return tex.sz().div(30);
-        } else {
-            return new Coord(1, 1);
+            }
+        } catch(Loading l) {
+
         }
+        return new Coord(1, 1);
+
     }
 }
